1ed342130faeee2d27c1692cf2fe0e6176730869
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2024-07-29 05:09:03 -0300

    add text interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBidingComponent } from './components/template/template-biding/template-biding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBidingComponent],
   template: `
     <!-- <router-outlet /> -->
     <app-new-component />
+    <app-template-biding/>
   `,
 })
 export class AppComponent {}
---
+++ src/app/components/template/template-biding/template-biding.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-biding',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-biding.component.html',
+  styleUrl: './template-biding.component.scss',
+})
+export class TemplateBidingComponent {
+  public name = 'Ivanildo Cândido';
+  public age = 37;
+}
