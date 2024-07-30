1e3ef9ed3231089457c42d047c796ba17dda20ff
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2024-07-30 05:10:50 -0300

    Add some initial properties

--- src/app/components/template/template-biding/template-biding.component.ts
+++ src/app/components/template/template-biding/template-biding.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-biding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-biding.component.html',
   styleUrl: './template-biding.component.scss',
 })
 export class TemplateBidingComponent {
   public name = 'Ivanildo Cândido';
-  public age = 37;
+  public age = 19;
+
+  public disabled = true;
+  public title = 'Cadastro';
+  public legend = this.age < 18 ? 'Menor de Idade' : 'Adulto';
+  public styleBold = this.age < 18 ? 'bold' : 'none';
+
+  public operation(operator: '+' | '-') {
+    if (operator === '+') {
+      return this.age++;
+    } else {
+      return this.age--;
+    }
+  }
+  public coordinates = '';
+  public onMouseMove(event: MouseEvent) {
+    return (this.coordinates = `X: ${event.clientX} Y: ${event.clientY}`);
+  }
 }
